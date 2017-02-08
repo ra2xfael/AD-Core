e3bffe43536e6bcb3af228f0b4f3315d67ea2d2b
Tigifan <tigifan21@gmail.com>
2017-02-08 19:43:47 +0100

    using backup thread in main

--- src/main/java/de/tigifan/core/bukkit/ADBukkit.java
+++ src/main/java/de/tigifan/core/bukkit/ADBukkit.java
@@ -2,14 +2,14 @@ package de.tigifan.core.bukkit;
 
 import de.apollodoener.api.ADPlugin;
 import de.tigifan.core.bukkit.commands.*;
+import de.tigifan.core.bukkit.timer.BackupTimer;
+import de.tigifan.core.bukkit.timer.HourTimer;
 import de.tigifan.core.bukkit.listeners.*;
-import de.tigifan.core.bukkit.runnable.BackupThread;
 import de.tigifan.core.bukkit.sql.RankManager;
 import de.tigifan.core.bukkit.sql.SQL;
 import de.tigifan.core.bukkit.util.Config;
 import de.tigifan.core.bukkit.util.ConfigType;
 import de.tigifan.core.bukkit.util.MessageUtil;
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
@@ -21,90 +21,100 @@ import java.util.HashMap;
  */
 public class ADBukkit extends JavaPlugin implements ADPlugin {
 
-    private static ADBukkit instance;
+  private static ADBukkit instance;
 
-    private String prefix = "§7[§aApolloDöner§7] ";
+  private String prefix = "§7[§aApolloDöner§7] ";
+  private HourTimer timer = new HourTimer();
 
-    private static HashMap<ConfigType, Config> configs = new HashMap<>();
+  private static HashMap<ConfigType, Config> configs = new HashMap<>();
 
-    private Thread backupThread = new BackupThread(Bukkit.getConsoleSender());
+  //private Thread backupThread = new BackupThread(Bukkit.getConsoleSender());
 
-    @Override
-    public void onEnable() {
-        MessageUtil.printConsoleMessage("&7Enabling plugin version &6" + this.getDescription().getVersion() + "&7!");
-        instance = this;
+  @Override
+  public void onEnable() {
+    MessageUtil.printConsoleMessage("&7Enabling plugin version &6" + this.getDescription().getVersion() + "&7!");
+    instance = this;
 
-        loadConfigs();
-        initalize();
+    loadConfigs();
+    initalize();
 
-        registerEvents();
-        registerCommands();
+    registerEvents();
+    registerCommands();
+    startTimer();
 
-        SQL.connect();
-        RankManager.createDatabase();
+    SQL.connect();
+    RankManager.createDatabase();
 
-        MessageUtil.printConsoleMessage("&7Plugin version &6" + this.getDescription().getVersion() + " &7enabled!");
+    MessageUtil.printConsoleMessage("&7Plugin version &6" + this.getDescription().getVersion() + " &7enabled!");
+  }
+
+  @Override
+  public void onDisable() {
+    //backupThread.interrupt();
+    SQL.disconnect();
+    MessageUtil.printConsoleMessage("&7Plugin disabled!");
+  }
+
+  private void loadConfigs() {
+    String[] files = {"config.yml", "messages.yml"};
+    String[] type = {"CONFIGURATION", "MESSAGES"};
+    for (int i = 0; i < 2; i++) {
+      MessageUtil.printConsoleMessage("&7Loading &6" + files[i] + "&7!");
+      Config config = new Config(files[i]);
+
+      configs.put(ConfigType.valueOf(type[i]), config);
     }
+  }
 
-    @Override
-    public void onDisable() {
-        backupThread.interrupt();
-        SQL.disconnect();
-        MessageUtil.printConsoleMessage("&7Plugin disabled!");
-    }
+  public static Config getConfig(ConfigType type) {
+    return configs.get(type);
+  }
 
-    private void loadConfigs() {
-        String[] files = {"config.yml", "messages.yml"};
-        String[] type = {"CONFIGURATION", "MESSAGES"};
-        for (int i = 0; i < 2; i++) {
-            MessageUtil.printConsoleMessage("&7Loading &6" + files[i] + "&7!");
-            Config config = new Config(files[i]);
+  private void registerEvents() {
+    this.getServer().getPluginManager().registerEvents(new PlayerJoinListener(), this);
+    this.getServer().getPluginManager().registerEvents(new PlayerLeaveListener(), this);
+    this.getServer().getPluginManager().registerEvents(new PlayerDeathListener(), this);
+    this.getServer().getPluginManager().registerEvents(new PlayerRespawnListener(), this);
+    this.getServer().getPluginManager().registerEvents(new SignChangeListener(), this);
+  }
 
-            configs.put(ConfigType.valueOf(type[i]), config);
-        }
-    }
+  private void registerCommands() {
+    this.getCommand("gamemode").setExecutor(new GamemodeCommand());
+    this.getCommand("heal").setExecutor(new HealCommand());
+    this.getCommand("feed").setExecutor(new FeedCommand());
+    this.getCommand("teleport").setExecutor(new TeleportCommand());
+    this.getCommand("setspawn").setExecutor(new SetspawnCommand());
+    this.getCommand("fly").setExecutor(new FlyCommand());
+    this.getCommand("spawn").setExecutor(new SpawnCommand());
+    this.getCommand("back").setExecutor(new BackCommand());
+    this.getCommand("rank").setExecutor(new RankManager());
+    this.getCommand("filedownload").setExecutor(new DownloadCommand());
+    this.getCommand("playerhead").setExecutor(new PlayerHeadCommand());
+    this.getCommand("backup").setExecutor(new BackupCommand());
+  }
 
-    public static Config getConfig(ConfigType type) {
-        return configs.get(type);
-    }
+  private void startTimer() {
+    timer.registerTimer(new BackupTimer());
 
-    private void registerEvents() {
-        this.getServer().getPluginManager().registerEvents(new PlayerJoinListener(), this);
-        this.getServer().getPluginManager().registerEvents(new PlayerLeaveListener(), this);
-        this.getServer().getPluginManager().registerEvents(new PlayerDeathListener(), this);
-        this.getServer().getPluginManager().registerEvents(new PlayerRespawnListener(), this);
-        this.getServer().getPluginManager().registerEvents(new SignChangeListener(), this);
-    }
+    timer.start();
+  }
 
-    private void registerCommands() {
-        this.getCommand("gamemode").setExecutor(new GamemodeCommand());
-        this.getCommand("heal").setExecutor(new HealCommand());
-        this.getCommand("feed").setExecutor(new FeedCommand());
-        this.getCommand("teleport").setExecutor(new TeleportCommand());
-        this.getCommand("setspawn").setExecutor(new SetspawnCommand());
-        this.getCommand("fly").setExecutor(new FlyCommand());
-        this.getCommand("spawn").setExecutor(new SpawnCommand());
-        this.getCommand("back").setExecutor(new BackCommand());
-        this.getCommand("rank").setExecutor(new RankManager());
-        this.getCommand("filedownload").setExecutor(new DownloadCommand());
-        this.getCommand("playerhead").setExecutor(new PlayerHeadCommand());
-        this.getCommand("backup").setExecutor(new BackupCommand());
-    }
+  public static ADBukkit getInstance() {
+    return instance;
+  }
 
-    public static ADBukkit getInstance() {
-        return instance;
-    }
+  private void initalize() {
+    prefix = ChatColor.translateAlternateColorCodes('&', ADBukkit.getConfig(ConfigType.MESSAGES).getConfig().getString("Prefix"));
+  }
 
-    private void initalize() {
-        prefix = ChatColor.translateAlternateColorCodes('&', ADBukkit.getConfig(ConfigType.MESSAGES).getConfig().getString("Prefix"));
-    }
+  @NotNull
+  @Override
+  public String getPrefix() {
+    return prefix;
+  }
 
-    @NotNull
-    @Override
-    public String getPrefix() {
-        return prefix;
-    }
-
-    @Override
-    public void setPrefix(String s) { this.prefix = s; }
+  @Override
+  public void setPrefix(String s) {
+    this.prefix = s;
+  }
 }
